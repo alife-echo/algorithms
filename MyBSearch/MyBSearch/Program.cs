e778c4d7b5dabdbda1123bddb17f8cca02b8a226
alife-echo <alifesilva90@gmail.com>
2025-01-21 23:11:45 -0300

    BinarySearch

---
+++ MyBSearch/MyBSearch/Program.cs
@@ -0,0 +1,9 @@
+﻿
+
+static void MyBs(int[] arr, int t)
+{
+    var high = arr.Length - 1;
+    var low = 0;
+
+    
+}
--- binary_search/binary_search_example/binary_search_example/Program.cs
+++ binary_search/binary_search_example/binary_search_example/Program.cs
@@ -1,3 +1,74 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+static int MySearchBinary(int[] numbers, int target)
+{
+    // a logica esta em manipular os indices de low e high caso o targe não corresponda a aposta(bet)
 
-Console.WriteLine("Hello, World!");
+    var low = 0;
+    var high = numbers.Length - 1;
+
+    while (low <= high)
+    {
+        var middle = (int)Math.Floor((double)(low + high)/2);
+        var bet = numbers[middle];
+
+        if (bet == target)
+            return middle;
+        if (bet > target)
+            high = middle - 1;
+        if (bet < target)
+            low = middle + 1;
+    }
+
+    return -1;
+}
+
+Console.WriteLine(MySearchBinary([1, 3, 5, 7, 9],3));
+
+
+
+static int SearchBinary(int[] list, int target )
+{
+    var low = 0;
+    var high = list.Length - 1; // 4
+    while (low <= high)
+    {
+        var middle = (int)Math.Floor((double)(low + high) / 2); // 2
+        var bet = list[middle]; // 5
+
+        if (bet == target) // 5 == 3 ?
+            return middle;
+        if (bet > target) // 5 > 3 ? 
+            high = middle - 1; // era 4  agora é 3
+        if(bet < target)
+            low = middle + 1;
+    }
+   
+    return -1;
+}
+
+Console.WriteLine(SearchBinary([1,3,5,7,9],9));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
